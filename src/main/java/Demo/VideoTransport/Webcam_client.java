2e4fc025821c3ea756e124e54468a502f355d28b
Z7\Qin <1507010127@s.upc.edu.cn>
2017-01-11 19:14:21 +0800

    为第一次成功的局域网视频传输代码添加注释

--- src/main/java/Demo/VideoTransport/Webcam_client.java
+++ src/main/java/Demo/VideoTransport/Webcam_client.java
@@ -12,25 +12,32 @@ import java.net.SocketException;
 
 /**
  * Created by 15070 on 2017/1/11.
+ *
+ * 这个是客户端，接收来自服务端的数据，然后转化为可以输出的图像，接着输出到屏幕
+ *
+ * 输出图像用到JPanel
  */
 public class Webcam_client extends JPanel{
-    static int ByteLength = 60000;
-    private DatagramSocket dgs;
-    private BufferedImage bufferedImage;
+    //这个类继承JPanel，所以可以渲染图像
 
-    private DatagramPacket dp;
-    private byte[] bs;
+    static int ByteLength = 60000;      //这个是用来接收数据的缓冲区大小，以后需要根据代码来改
+    private DatagramSocket dgs;         //UDP套接字
+    private BufferedImage bufferedImage;    //用来装 转换后的图像
 
+    private byte[] bs;                  //用来接收数据的缓冲区
+    private DatagramPacket dp;          //封装了上面哪个 bs， 可以用来装载UDP收到的数据
+
+    //构造函数，传入端口表示监听哪个端口数据，
     public Webcam_client(){
         try {
-            dgs = new DatagramSocket(8889);
+            dgs = new DatagramSocket(8889);     //默认监听8889端口
         } catch (SocketException e) {
             e.printStackTrace();
         }
-        init();
+        init();     //解决了端口问题，执行这个方法实现初始化
     }
 
-    public Webcam_client(int port){
+    public Webcam_client(int port){     //上面哪个的有参构造（参数就是监听的端口号）
         try {
             dgs = new DatagramSocket(port);
         } catch (SocketException e) {
@@ -39,45 +46,55 @@ public class Webcam_client extends JPanel{
 
         init();
     }
+
+    //初始化设置，把缓冲区设置好
     private void init(){
-        bs = new byte[2*ByteLength];
-        dp = new DatagramPacket(bs, bs.length);
+        bs = new byte[2*ByteLength];        //申请一个字节数组，长度是 2 * 预先设置好的缓冲区长度
+        dp = new DatagramPacket(bs, bs.length);     //把bs封装成 UDP数据包类
     }
 
 
-    public BufferedImage getImage(){
+    public BufferedImage getImage(){        //接收UDP数据，然后转化为图像保存在bufferedImage
         try {
-            dgs.receive(dp);
-            ByteArrayInputStream in = new ByteArrayInputStream(bs);
-            bufferedImage = ImageIO.read(in);
+            dgs.receive(dp);            //接收 收到的数据
+            ByteArrayInputStream in = new ByteArrayInputStream(bs);     //通过流处理，转化为数据流
+            bufferedImage = ImageIO.read(in);       //通过ImageIO的read方法转化为 图像
         } catch (IOException e) {
             e.printStackTrace();
         }
             return bufferedImage;
     }
 
-    public void paint(Graphics g)
+    public void paint(Graphics g)       //绘制函数
     {
         super.paint(g);
+
+        //把bufferedImage绘制出来
         g.drawImage(bufferedImage,0,0,
                     bufferedImage.getWidth(),
-                    bufferedImage.getHeight(),null);
+                    bufferedImage.getHeight(),
+                null);
     }
 
     public static void main(String[] args) {
+        //先新建一个类，然后先获取一下图像，初始化一下bufferedImage
         Webcam_client wclient = new Webcam_client(8889);
         wclient.getImage();
 
+        //新建一个画板，并且初始化
         JFrame w = new JFrame();
-        w.setSize(wclient.getImage().getWidth(),wclient.getImage().getHeight());
+        w.setSize(wclient.getImage().getWidth(), wclient.getImage().getHeight());
 
+        //把 wclient 添加到画板上面，并且让画板的属性设置成可见
         w.add(wclient);
         w.setVisible(true);
 
         while(true){
-            wclient.getImage();
-            wclient.repaint();
+            //循环
+            wclient.getImage();     //接收数据
+            wclient.repaint();      //刷新画板
 
+            //休眠50毫秒
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
--- src/main/java/Demo/VideoTransport/Webcam_server.java
+++ src/main/java/Demo/VideoTransport/Webcam_server.java
@@ -13,19 +13,26 @@ import java.net.SocketException;
 
 /**
  * Created by 15070 on 2017/1/11.
+ *
+ * 第一次完成视频流在局域网传输，在这里做个笔记
+ *
+ * 使用方法，打开 Webcam_server和Webcam_client即可
+ *
+ *
  */
 public class Webcam_server {
-    BufferedImage bufferedImage;
-    Webcam webcam;
+    BufferedImage bufferedImage;        //图像数据
+    Webcam webcam;                      //摄像头
 
-    DatagramSocket dso;
+    DatagramSocket dso;                 //UDP套接字（传输用）
 
 
+    //下面两个是构造函数，传入套接字的使用端口（没有时候就默认用 8888号端口）
     public Webcam_server(){
 
-        webcam = Webcam.getDefault();
+        webcam = Webcam.getDefault();       //获取摄像头
         try {
-            dso = new DatagramSocket(8888);
+            dso = new DatagramSocket(8888);     //申请一个套接字，并且绑定一个默认端口 8888
         } catch (SocketException e) {
             e.printStackTrace();
         }
@@ -34,12 +41,14 @@ public class Webcam_server {
     public Webcam_server(int port){
         webcam = Webcam.getDefault();
         try {
-            dso = new DatagramSocket(port);
+            dso = new DatagramSocket(port);         //套接字绑定传入的端口
         } catch (SocketException e) {
             e.printStackTrace();
         }
     }
 
+
+    //启动摄像头
     public boolean webcam_open(){
         return this.webcam.open();
     }
@@ -48,13 +57,15 @@ public class Webcam_server {
         return bufferedImage;
     }
 
+    //让摄像头拍摄一张照片，并且把照片存放到 bufferedImage里面
     public boolean capture(){
         bufferedImage = webcam.getImage();
         return bufferedImage==null?false:true;
     }
 
+    //发送 bufferedImage ， 传入的参数是发送目标的地址
     public void sendImage(InetSocketAddress addr){
-        ByteArrayOutputStream os=new ByteArrayOutputStream();
+        ByteArrayOutputStream os=new ByteArrayOutputStream();       //java io技术，申请一个处理流
         try {
             ImageIO.write(bufferedImage,"PNG",os);
             byte[] image_buf = os.toByteArray();
@@ -70,15 +81,18 @@ public class Webcam_server {
     }
 
     public static void main(String[] args) {
-        Webcam_server server = new Webcam_server(8888);
-        server.webcam_open();
+        Webcam_server server = new Webcam_server(8888);         //实例化一个server类
+        server.webcam_open();           //启动摄像头
 
+        //设置传输地址
         InetSocketAddress clientaddr = new InetSocketAddress("127.0.0.1",8889);
 
         while (true){
-            server.capture();
-            server.sendImage(clientaddr);
+            //循环发送
+            server.capture();       //拍摄
+            server.sendImage(clientaddr);       //发送
 
+            //系统休眠50毫秒
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
@@ -86,7 +100,6 @@ public class Webcam_server {
             }
 
         }
-
 
     }
 
