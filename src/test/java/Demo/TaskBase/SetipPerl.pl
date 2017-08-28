19988a9fad9a0f5058e223579b9ca04d63a35e89
HongHem.Hu <1507010127@s.upc.edu.cn>
2017-08-28 16:43:08 +0800

    upload Perl ifconfig script

--- src/test/java/Demo/TaskBase/SetipPerl.pl
+++ src/test/java/Demo/TaskBase/SetipPerl.pl
@@ -1,38 +1,50 @@
 #!/usr/bin/perl
-# 给网卡添加ip地址脚本
 
 use utf8;
+use strict;
 use 5.010;
+use JSON;
+use Data::Dumper;
 
-my ($dev, $ipgroup) = @ARGV;
+my ( $JSONpath, $dev, $ipgroup, ) = @ARGV;
 
-my $ipstart;
-given ($ipgroup) {
-    when($_ eq "ip1") {$ipstart = 150;}
-    when($_ eq "ip2") {$ipstart = 170;}
-    default {die "error input ipstart";}
-};
+open my $Conft, '<', $JSONpath or die $!;
+my $jsonString;
+$jsonString .= $_ while ( $_ = <$Conft> );
+close $Conft;
 
-my $ipbase='192.168.31.';       # 网络段
-my $netmask = "255.255.255.0";  # 子网掩码
-my $end=9;                      # 分配数量（9 表示 分配10 个 0..9)
+my $jsonObj = decode_json $jsonString;
+die "can't find ipgroup $ipgroup, $!" unless $jsonObj->{$ipgroup};
 
-my @wlanip;
-
-for my $index (0..$end) {
-    my $ip = $ipbase.($ipstart + $index);
-    push @wlanip,$ip;
-    #system("ifconfig ".$dev.":".$index." ".$ip." netmask ".$netmask." up");
-    #say "ifconfig $dev:$index $ip netmask $netmask up";
+my $netmask = "255.255.255.0";    # 子网掩码
+my $index   = 0;
+for my $ip ( @{ $jsonObj->{$ipgroup} } ) {
     system("ifconfig $dev:$index $ip netmask $netmask up");
+    say $ip;
+    $index++;
 }
 
-for my $index (@wlanip) {
-    say "\"$index\",";
-}
+#system("ifconfig $dev:$index $ip netmask $netmask up");
+
+#my $ipbase='192.168.31.';       # 网络段
+#my $end=9;                      # 分配数量（9 表示 分配10 个 0..9)
+
+#my @wlanip;
+
+#for my $index (0..$end) {
+#my $ip = $ipbase.($ipstart + $index);
+#push @wlanip,$ip;
+##system("ifconfig ".$dev.":".$index." ".$ip." netmask ".$netmask." up");
+##say "ifconfig $dev:$index $ip netmask $netmask up";
+#system("ifconfig $dev:$index $ip netmask $netmask up");
+#}
+
+#for my $index (@wlanip) {
+#say "\"$index\",";
+#}
 
 #say "-----------------------";
 
 #for my $index (@wlanip2) {
-    #say "\"$index\",";
+#say "\"$index\",";
 #}
